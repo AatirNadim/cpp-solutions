0784df7a1f9073f78aa61d7703712667e371e8b6
AatirNadim <aatir.nadim@gmail.com>
2022-12-24 18:22:44 +0000

    leetcode daily must read

--- domino_and_tromino.cpp
+++ domino_and_tromino.cpp
@@ -46,3 +46,28 @@ public:
         return (int)(dp[n]%val);
     }
 };
+
+// O(n) time O(1) space
+
+#define ll long long int
+class Solution {
+public:
+    int numTilings(int n) {
+        vector<ll> dp(4);
+        int val = 1e9 + 7;
+        if(n < 3) return n;
+        vector<ll> sum_dp(4);
+        dp[0] = 1; dp[1] = 1, dp[2] = 2, dp[3] = 5;
+        sum_dp[0] = 1, sum_dp[1] = 2, sum_dp[2] = 4, sum_dp[3] = 9;
+        for(int i=4;i<=n;i++) {
+            dp[i%4] = (dp[(i-1)%4]%val + dp[(i-2)%4]%val)%val;
+            
+            int val1 = (sum_dp[(i-3)%4]%val + sum_dp[(i-3)%4]%val)%val;
+            
+            dp[i%4] = (dp[i%4]%val + val1%val)%val;
+            
+            sum_dp[i%4] = (sum_dp[(i-1)%4]%val + dp[i%4]%val)%val;
+        }
+        return (int)(dp[n%4]%val);
+    }
+};
