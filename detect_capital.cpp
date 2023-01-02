7360193dfe5ce60cdc05d9be53c1e86cab5f8d0e
AatirNadim <aatir.nadim@gmail.com>
2023-01-02 15:59:02 +0000

    leetcode daily

---
+++ detect_capital.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/detect-capital/description/
+
+// https://leetcode.com/submissions/detail/869636534/
+
+
+class Solution {
+    public:
+    bool isCap(char c) {
+        return c >= 'A' and c <= 'Z';
+    }
+    bool main_func(string word) {
+        int n = word. size(), cap = 0;
+        for (char &c : wor) 
+    }
