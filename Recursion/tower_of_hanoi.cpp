99b9b0eb82ca2bc00649014464ed75e01072da7b
AatirNadim <aatir.nadim@gmail.com>
2023-01-17 14:14:46 +0000

    simple approach

--- Recursion/tower_of_hanoi.cpp
+++ Recursion/tower_of_hanoi.cpp
@@ -1,5 +1,9 @@
 // here, out of the three rods, rod 2 is the destination, rod 1 is the current rod, where the discs are stacked and rod 3 is the spare rod.
 
+// 1.for nth rod, 1st rod being the largest rod, move the stack of (n-1) rods to the spare rod
+// 2. move the nth rod to the dest_rod
+// 3. move the stack of (n-1) rods from the spare rod to the dest_rod.
+
 #include <bits/stdc++.h>
 using namespace std;
 void move(int start, int end, int curr_tower, int dest_tower, int spare_tower) {
