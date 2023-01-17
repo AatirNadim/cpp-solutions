f5ebd6601f0113ae8e6dad6daf4e4f7e4882955d
AatirNadim <aatir.nadim@gmail.com>
2023-01-17 14:13:02 +0000

    tower of hanoi simple implementation

---
+++ Recursion/tower_of_hanoi.cpp
@@ -0,0 +1,16 @@
+// here, out of the three rods, rod 2 is the destination, rod 1 is the current rod, where the discs are stacked and rod 3 is the spare rod.
+
+#include <bits/stdc++.h>
+using namespace std;
+void move(int start, int end, int curr_tower, int dest_tower, int spare_tower) {
+    if(start > end) return;
+    move(start + 1, end, curr_tower, spare_tower, dest_tower);
+    cout<<"Disc --> "<<start<<"\nmoved from "<<curr_tower<<" to "<<dest_tower<<"\n";
+    move(start + 1, end, spare_tower, dest_tower, curr_tower);
+}
+int main() {
+    cout<<"Give the number of discs for the tower of hanoi"<<endl;
+    int n;cin>>n;
+    move(1, n, 0, 1, 2);
+    return 0;
+}
