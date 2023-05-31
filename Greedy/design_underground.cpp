be2f6b6caeada3b163a7102b9e0274f765fb454b
AatirNadim <aatir.nadim@gmail.com>
2023-05-31 08:28:57 +0000

    leetcode daily

---
+++ Greedy/design_underground.cpp
@@ -0,0 +1,33 @@
+// problem link --> https://leetcode.com/problems/design-underground-system/
+
+// my solution link --> https://leetcode.com/problems/design-underground-system/discuss/3581622/C%2B%2B-oror-HashMap-oror-Short-and-Simple
+
+class UndergroundSystem {
+public:
+    unordered_map<int, pair<string, int>> start;
+    unordered_map<string, unordered_map<string, int>> total, times;
+    UndergroundSystem() {
+        start.clear(), total.clear(), times.clear();
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        start[id] = { stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        total[start[id].first][stationName] += t - start[id].second;
+        times[start[id].first][stationName]++;
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        return (total[startStation][endStation])/(double)(times[startStation][endStation]);
+    }
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
