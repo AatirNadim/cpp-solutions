b2b121e39e2578a999a94a70d9e91db4c5835da9
AatirNadim <100563812+AatirNadim@users.noreply.github.com>
2024-12-16 18:15:46 +0000

    peak element

--- Arrays/Binary_Search/peak_element.cpp
+++ Arrays/Binary_Search/peak_element.cpp
@@ -1,23 +1,24 @@
-// find the peak element in the given array
-#include <bits/stdc++.h>
-using namespace std;
-bool peak(vector<int>& v, int idx) {
-    if(!idx) return v[idx] > v[idx + 1];
-    if(idx == v.size() -1) return v[idx] > v[idx -1];
-    return (v[idx] > v[idx - 1] and v[idx] > v[idx + 1]);
-}
-int main() {
-    vector<int> v({30, 20, 15, 2, 98 , 90, 67});
-    // vector<int> v({1, 2, 3, 4, 5, 6, 7, 8, 9});
-    int minval = 0, maxval= v.size() - 1, medval;
-    while(minval < maxval) {
-        medval = minval + ((maxval - minval)>>1);
-        if(peak(v, medval)) {
-            cout<<medval<<" "<<v[medval]; return 0;
-        }
-        if(v[medval] < v[medval + 1]) minval = medval + 1;
-        else maxval = medval - 1;
+/*
+    problem link --> https://www.geeksforgeeks.org/problems/peak-element/1
+*/
+
+class Solution {
+    
+    bool isPeak(vector<int>& arr, int idx) {
+        return (!idx or arr[idx] > arr[idx - 1]) and 
+        (idx == arr.size() - 1 or arr[idx] > arr[idx + 1]);
     }
-    cout<<minval<<" "<<v[minval]<<"\n"<<maxval<<" "<<v[maxval];
-    return 0;
-}
+    
+  public:
+    int peakElement(vector<int> &arr) {
+        // Your code here
+        int minval = 0, maxval = arr.size() - 1, medval;
+        while(minval <= maxval) {
+            medval = minval + (maxval - minval)/2;
+            if(isPeak(arr, medval)) return medval;
+            else if(medval and arr[medval - 1] > arr[medval]) maxval = medval - 1;
+            else minval = medval + 1;
+        }
+        return medval;
+    }
+};
